d3a56027cb294c419d1168847fee4e8f43780d3b
Neak Panhboth <neakpanhboth18@kit.edu.kh>
2022-09-10 12:42:35 +0700

    added animation

--- components/Index/HeroSection.vue
+++ components/Index/HeroSection.vue
@@ -9,14 +9,19 @@
     <!-- the text for the hero section-->
     <div class="text-white text-center w-hero_text">
       <!-- main text -->
-      <div class="font-bold lg:text-5xl text-3xl" id="hero-title">
+      <div
+        class="font-bold lg:text-5xl text-3xl"
+        id="hero-title"
+        data-aos="zoom-in-up"
+        data-aos-duration="1400"
+      >
         {{ $t("home.hero_section.title1") }}<br />
         {{ $t("home.hero_section.title2") }} <br />
         {{ $t("home.hero_section.title3") }} <br />
       </div>
       <!-- subtitle text -->
       <div class="font-thin">{{ $t("home.hero_section.subtitle") }}</div>
-      <div class="mt-4">
+      <div class="mt-4" data-aos="fade-up" data-aos-duration="1200">
         <nuxt-link to="/contact">
           <button
             class="bg-primary hover:bg-opacity-0 border border-primary px-10 py-2 text-white hover:text-primary mr-2 transition ease-in-out duration-700"
